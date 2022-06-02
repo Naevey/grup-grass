2e05d079d1aee177974c92e7f95129b88dc57d07
Naevey <fpsgaaa@gmail.com>
2022-06-02 11:30:11 -0700

    i hab no idea. i think upload works

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template, request
+from flask import render_template, redirect, request, url_for, send_from_directory
 from flask_login import login_required
 
 from __init__ import app
@@ -9,8 +9,9 @@ from cruddy.app_crud import app_crud
 from cruddy.app_crud_api import app_crud_api
 from krug.query import students_all
 from content import app_content
+from uploady.app_upload import app_upload
 
-
+app.register_blueprint(app_upload)
 app.register_blueprint(app_krug)
 app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
@@ -26,10 +27,6 @@ def index():
 def calender():
     return render_template("calendar.html")
 
-@app.route('/upload')
-@login_required
-def upload():
-    return render_template("upload.html", table=students_all() )
 @app.route('/studentworks')
 @login_required
 def studentworks():
@@ -61,6 +58,18 @@ def greet():
             return render_template("greet.html", name=name)
     # starting and empty input default
     return render_template("greet.html", name="World")
+
+# serve uploaded files so they can be downloaded by users.
+@app.route('/uploads/<name>')
+def uploads_endpoint(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
+# register "uploads_endpoint" endpoint so url_for will find all uploaded files
+app.add_url_rule(
+    "/" + app.config['UPLOAD_FOLDER'] + "/<name>", endpoint="uploads_endpoint", build_only=True
+)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
